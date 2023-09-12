c7ef58a334ef3d4756f4ec654fbf721bfea4b8ef
Dimi Vaksman <112968050+DimiVaksman@users.noreply.github.com>
2023-09-12 18:52:36 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import App from './Components/App';
+import {App} from './Components/App';
 import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { PersistGate } from 'redux-persist/integration/react';
