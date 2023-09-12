624bf4f7731393998d7c33666507a609f26044dc
Dimi Vaksman <112968050+DimiVaksman@users.noreply.github.com>
2023-09-12 17:42:44 +0200

    phonebook hw

--- src/index.js
+++ src/index.js
@@ -1,20 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
-import { store } from './redux/store';
 import { Provider } from 'react-redux';
-import { BrowserRouter } from "react-router-dom";
+import App from './Components/App';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './redux/store';
+import { GlobalStyle } from './Components/GlobalStyle';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  
-  <BrowserRouter basename="/goit-react-hw-08-phonebook">
-  <Provider store={store}>
-    <React.StrictMode>
-
-      <App />
-
-    </React.StrictMode>
-  </Provider>
-  </BrowserRouter>
+  <React.StrictMode>
+    <BrowserRouter basename="/goit-react-hw-08-phonebook">
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+          <GlobalStyle />
+        </PersistGate>
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>
 );
